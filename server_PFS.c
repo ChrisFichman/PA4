de8df88c884a537b30f1db7ffb5be2c578c354c1
ChrisFichman <christopher.fichman@colorado.edu>
2013-12-07 15:30:34 -0700

    Setup

---
+++ server_PFS.c
@@ -0,0 +1,26 @@
+// Name: Chris Fichman
+// Email: chris.fichman@gmail.com
+// Repo: https://github.com/ChrisFichman/PA4
+// Created: December 2, 2013
+// Modified: December 6, 2013
+
+// File: server_PFS.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>  
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include "util.h"
